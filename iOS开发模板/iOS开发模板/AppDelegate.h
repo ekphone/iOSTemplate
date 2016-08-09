fd52eeec751ba2699840521cc092cf3a4611f4d5
黄驿 <workhuangdl@huangdldemacbook-pro.local>
2016-08-09 12:34:46 +0800

    初始化

---
+++ iOS开发模板/iOS开发模板/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  iOS开发模板
+//
+//  Created by 黄栋梁 on 16/8/9.
+//  Copyright © 2016年 黄栋梁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ iOS开发模板/iOS开发模板/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS开发模板
+//
+//  Created by 黄栋梁 on 16/8/9.
+//  Copyright © 2016年 黄栋梁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
