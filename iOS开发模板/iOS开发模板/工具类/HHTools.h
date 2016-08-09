e87ca725fbe2ca9bb222e73a246ac2cbe51e13e7
ekphone <workhuangdl@huangdldemacbook-pro.local>
2016-08-09 13:03:43 +0800

    工具类

---
+++ iOS开发模板/iOS开发模板/工具类/HHTools.h
@@ -0,0 +1,13 @@
+//
+//  HHTools.h
+//  iOS开发模板
+//
+//  Created by 黄栋梁 on 16/8/9.
+//  Copyright © 2016年 黄栋梁. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HHTools : NSObject
+
+@end
---
+++ iOS开发模板/iOS开发模板/工具类/HHTools.m
@@ -0,0 +1,13 @@
+//
+//  HHTools.m
+//  iOS开发模板
+//
+//  Created by 黄栋梁 on 16/8/9.
+//  Copyright © 2016年 黄栋梁. All rights reserved.
+//
+
+#import "HHTools.h"
+
+@implementation HHTools
+
+@end
